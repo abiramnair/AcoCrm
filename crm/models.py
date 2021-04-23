7d545e2ce63ba307c5e1163068ca84eaf3220bbc
abiramnair <abiramnair@gmail.com>
2021-04-23 12:47:59 +0800

    testing gitignore db.sqlite3

--- crm/models.py
+++ crm/models.py
@@ -108,6 +108,9 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+    def __repr__(self):
+        return self.name
+
 
 class Sale(models.Model):
     staff = models.ForeignKey(
